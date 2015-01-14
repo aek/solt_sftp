61fdedd5af14ecca32bd7ab0afa5cef6bca0a9bd
Axel Mendoza <aekroft@gmail.com>
2015-01-14 12:34:18 -0500

    check for ids and users, also delete non existing ids

--- src/solt_sftp/broker.py
+++ src/solt_sftp/broker.py
@@ -51,22 +51,35 @@ class solt_broker(object):
         self.root_folder = config.get('sftp_path','/opt/solt_sftp/files')
         
         self.authorized_keys = {}
+        self.ids_to_users = {}
         
         self.sftp_user_ids = self.redis_conn.smembers('solt.sftp.users')
         if self.sftp_user_ids:
             for user_id in self.sftp_user_ids:
-                self.on_channel_user_create({'data':user_id})
+                self.on_channel_user_handle({'data':user_id})
         else:
             self.sftp_user_ids = []
         
         self.subscriber = self.redis_conn.pubsub()
         self.subscriber_greenlet = gevent.spawn(self.listener)
     
-    def on_channel_user_create(self, message):
-        user_key = 'solt.sftp.user.%s' % message.get('data')
+    def on_channel_user_handle(self, message):
+        user_id = message.get('data')
+        user_key = 'solt.sftp.user.%s' % user_id
         user_ssh_key = user_key+'.keys'
         user_obj = self.redis_conn.hgetall(user_key)
+        
         if user_obj:
+            if self.ids_to_users.get(user_id,False) and \
+                    self.authorized_keys.get(user_obj.get('name'), False) and \
+                    self.authorized_keys.get(user_obj.get('name')).get('id') != user_id: 
+                existing_user = self.ids_to_users.get(self.authorized_keys.get(user_obj.get('name')).get('id'), False) #username of mapped id
+                if existing_user and existing_user != user_obj.get('name'):
+                    _logger.error("Already existing username with different ID - actual: (id,%s => user,%s) new: (user,%s => id,%s) -", user_id, self.ids_to_users[user_id], user_obj.get('name'), self.authorized_keys[user_obj.get('name')].get('id'))
+                    return
+            else:
+                if self.ids_to_users.get(user_id,False):
+                    self.ids_to_users[user_id] = user_obj.get('name')
             if user_obj.get('folder', False):
                 folder = user_obj.get('folder')
             else:
@@ -77,20 +90,25 @@ class solt_broker(object):
                 os.makedirs(real_path)
             user_ssh = self.redis_conn.smembers(user_ssh_key)
             self.authorized_keys[user_obj.get('name')] = {
+                'id': user_id,
+                'name': user_obj.get('name'),
                 'ssh-keys': tuple(user_ssh),
                 'active': user_obj.get('active'),
                 'folder': folder,
             }
-    
+        elif self.ids_to_users.get(user_id,False):
+            self.authorized_keys.pop(self.ids_to_users.get(user_id), False)
+            self.ids_to_users.pop(user_id, False)
+
     def listener(self):
         self.subscriber.subscribe(self.channel)
         try:
             try:
                 while True:
                     for msg in self.subscriber.listen():
-                        self.on_channel_user_create(msg)
+                        self.on_channel_user_handle(msg)
             except redis.ConnectionError, e:
-                if e.message not in EXPECTED_CONNECTION_ERRORS:  # Hm, there's no error code, only the message
+                if e.message not in EXPECTED_CONNECTION_ERRORS:
                     _logger.info('Caught `%s`, will quit now', e.message)
                     raise
         except KeyboardInterrupt:
